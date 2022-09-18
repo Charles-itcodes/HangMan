2013b1029a846b68c90063ad453a6bad48a2aa27
Charles <rudahushac@gmail.com>
2022-09-18 22:05:26 +0200

    Add project files.

---
+++ HangMan/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HangMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Hello";
+            string guessedWord = "";
+            int count = 0;
+            int countLimit = 10;
+            bool isChansesOver = false;
+
+            while (guessedWord != secretWord && !isChansesOver)
+            {
+                if (count < countLimit)
+                {
+                    Console.WriteLine("Guess the Word: ");
+                    guessedWord = Console.ReadLine();
+                    count++;
+                }
+                else
+                {
+                    isChansesOver = true;
+
+                }
+            }
+            if (isChansesOver)
+            {
+                Console.WriteLine("Sorry! You Lose, Game is over!");
+            }
+            else
+            {
+                Console.WriteLine("Congs You Win!");
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
